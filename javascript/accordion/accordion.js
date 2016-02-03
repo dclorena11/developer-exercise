6cd751506ec6bcd622323d2ea02ac71d9bd1d31d
dclorena11 <dclorena11@gmail.com>
2016-02-03 17:55:59 -0600

    other divs close when you open a new one

--- javascript/accordion/accordion.js
+++ javascript/accordion/accordion.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   $('.accordion-header > div').hide();
 
   $('.accordion-header').on("click", function(){
-    $(this).children()  .show("slow");
+    $('.accordion-header > div').hide("slow");
+    $(this).children().show("slow");
   })
 
 });
