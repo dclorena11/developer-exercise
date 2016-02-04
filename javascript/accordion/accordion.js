5f3d2c5db9400337164cc7a16ddb38ab34daba0a
dclorena11 <dclorena11@gmail.com>
2016-02-03 20:53:33 -0600

    have div changing different colors depending up down

--- javascript/accordion/accordion.js
+++ javascript/accordion/accordion.js
@@ -1,17 +1,42 @@
 $(document).ready(function(){
-  $('.accordion-header > div').hide();
+
 
   $('.accordion-header').on("click", function(){
-    $('.accordion-header > div').hide("slow");
-    $(this).children().show("slow");
+    $('.accordion-header > div').removeClass("active").hide("slow");
+    $(this).find("div").addClass("active").show("slow");
   })
 
   $(document).on("keydown", function(e){
     if(e.which == 38){
-      alert("up");
+      var target = $('active');
+      $('.active').css("color", "red");
     }else if(e.which == 40){
-      alert("down");
+      $('.active').css("color", "green");
     }
   })
 
+  //   $(".list-item").bind({
+  //   keydown: function(e) {
+  //     var key = e.keyCode;
+  //     var target = $(e.currentTarget);
+
+  //     switch(key) {
+  //       case 38: // arrow up
+  //         target.prev().focus();
+  //         break;
+  //       case 40: // arrow down
+  //         target.next().focus();
+  //         break;
+  //     }
+  //   },
+
+  //   focusin: function(e) {
+  //     $(e.currentTarget).addClass("selected");
+  //   },
+
+  //   focusout: function(e) {
+  //     $(e.currentTarget).removeClass("selected");
+  //   }
+  // });
+  // $("li").first().focus();
 });
