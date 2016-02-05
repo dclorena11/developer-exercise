eff212b64862530a10a9c32461feb67ac7e67f56
dclorena11 <dclorena11@gmail.com>
2016-02-04 19:02:07 -0600

    try to bind

--- javascript/accordion/accordion.js
+++ javascript/accordion/accordion.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 
   $('.accordion-header > div').hide();
 
-  $('.accordion-header').on("click", function(){
-    $('.accordion-header > div').hide("slow");
-    $(this).find("div").show("slow");
-  })
+  $('.accordion-header').on("click",
+    function(){
+      $('.accordion-header > div').hide("slow");
+      $(this).find("div").show("slow").focus();
+    })
 
   $("div:visible").closest(".accordion-header").on("keydown", function(e){
     var target = $(this).first("div");
@@ -20,10 +21,35 @@ $(document).ready(function(){
     }
   })
 
-  $(".accordion-header").bind({
-    keydown: function(e) {
-  },
+    $(".accordion-header").bind({
+      keydown: function(e) {
+    },
 
-  });
+});
+  // $("div:visible").closest(".accordion-header").bind({
+  //   keydown: function(e) {
+  //     var key = e.keyCode;
+  //     var target = $(e.currentTarget);
+
+  //     switch(key) {
+  //       case 38: // arrow up
+  //         alert("up");
+  //         target.prev().focus();
+  //         break;
+  //       case 40: // arrow down
+  //         target.next().focus();
+  //         break;
+  //     }
+  //   },
+
+  //   focusin: function(e) {
+  //     $(e.currentTarget).children("div").show("slow");
+  //   },
+
+  //   focusout: function(e) {
+  //     $(e.currentTarget).children("div").hide("slow");
+  //   }
+  // });
+  // $("li").first().focus();
 
 });
