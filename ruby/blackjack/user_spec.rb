9fc4da08f6f096d4fe5a46f8d2b10ad9131c1158
dclorena11 <dclorena11@gmail.com>
2016-02-08 11:14:57 -0600

    add blackjack rspec gem and file

---
+++ ruby/blackjack/user_spec.rb
@@ -0,0 +1,17 @@
+require_relative 'blackjack'
+
+describe User do
+  let(:user) { User.new }
+
+  #You'll need to switch `pending` to `describe` when you're ready
+  # to implement each set of tests.
+
+  describe '' do
+  end
+
+  describe '' do
+  end
+
+
+
+end
