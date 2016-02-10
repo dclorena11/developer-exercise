e8a7b34fa68675069906681af1ee8445a2275a52
dclorena11 <dclorena11@gmail.com>
2016-02-10 15:38:08 -0600

    few changes

--- javascript/simple_backbone/quotes.js
+++ javascript/simple_backbone/quotes.js
@@ -1,9 +1,32 @@
 var PageableCollection = Backbone.PageableCollection;
 
-var Quote = Backbone.PageableCollection.extend({});
+//Model
+var Quote = Backbone.PageableCollection.extend({
 
-var Quotes = Backbone.PageableCollection.extend({
-  model: Quote,
-  url: "https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json"
 });
 
+//Collection
+var Quotes = Backbone.PageableCollection.extend({
+  model: Quote,
+  url: "https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json",
+  mode: "client",
+  state: {
+    pageSize: 15
+  }
+});
+
+//fetch not working
+var quotes = new Quotes();
+quotes.fetch();
+console.log(quotes);
+
+
+//QuoteView
+// var QuoteView = Backbone.View.extend({
+//   className = "quote"
+// });
+
+// quoteView = new QuoteView({collection: quote});
+// quote.fetch().done(function(){
+//   quoteView.render();
+// })
