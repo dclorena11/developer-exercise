22c6a08cf15d96d8d70ee4fd110d5f2430bb9a2b
dclorena11 <dclorena11@gmail.com>
2016-02-11 12:21:21 -0600

    add to QuoteView

--- javascript/simple_backbone/quotes.js
+++ javascript/simple_backbone/quotes.js
@@ -1,32 +1,30 @@
 var PageableCollection = Backbone.PageableCollection;
 
 //Model
-var Quote = Backbone.PageableCollection.extend({
-
-});
+var Quote = Backbone.PageableCollection.extend({});
 
 //Collection
 var Quotes = Backbone.PageableCollection.extend({
   model: Quote,
   url: "https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json",
-  mode: "client",
   state: {
     pageSize: 15
   }
 });
 
-//fetch not working
-var quotes = new Quotes();
-quotes.fetch();
-console.log(quotes);
-
+var quotes = new Quotes([],
+  {mode: "client"}
+);
 
 //QuoteView
-// var QuoteView = Backbone.View.extend({
-//   className = "quote"
-// });
+var QuoteView = Backbone.View.extend({
+  className:"quote",
+  template: _.template("Update template to show quote attributes"),
+  fetchQuotes: function(){
+    this.collection.getFirstPage();
+    console.log(this.collection);
+  }
+});
 
-// quoteView = new QuoteView({collection: quote});
-// quote.fetch().done(function(){
-//   quoteView.render();
-// })
+var quoteView = new QuoteView({collection: quotes});
+quoteView.fetchQuotes();
