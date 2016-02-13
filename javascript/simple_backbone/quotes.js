2bbe89296212a5fec07b672111b1d87da190344a
dclorena11 <dclorena11@gmail.com>
2016-02-13 13:41:18 -0600

    finally fetch collection using pagination

--- javascript/simple_backbone/quotes.js
+++ javascript/simple_backbone/quotes.js
@@ -1,30 +1,23 @@
 var PageableCollection = Backbone.PageableCollection;
 
 //Model
-var Quote = Backbone.PageableCollection.extend({});
+var Quote = Backbone.Model.extend({});
 
-//Collection
 var Quotes = Backbone.PageableCollection.extend({
   model: Quote,
-  url: "https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json",
+  url: "https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json"
+});
+
+var quotes = new Quotes([], {
+  mode: "client",
   state: {
+    firstPage: 0,
+    currentPage: 0
+  },
+  queryParams: {
     pageSize: 15
   }
 });
 
-var quotes = new Quotes([],
-  {mode: "client"}
-);
-
-//QuoteView
-var QuoteView = Backbone.View.extend({
-  className:"quote",
-  template: _.template("Update template to show quote attributes"),
-  fetchQuotes: function(){
-    this.collection.getFirstPage();
-    console.log(this.collection);
-  }
-});
-
-var quoteView = new QuoteView({collection: quotes});
-quoteView.fetchQuotes();
+quotes.getFirstPage({fetch: true});
+console.log(quotes);
